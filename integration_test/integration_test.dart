e3049f060ba4e0299827cf4b6b2c66c129eecb36
Hafiz Muhammad Huzaifa <muhammadhuzaifa5200@gmail.com>
2025-04-14 17:21:29 +0500

    some fixes

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:testing_flutter/main.dart';
+import 'package:testing_flutter/main.dart'  as app;
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -8,8 +8,10 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Counter increments, decrements, and resets correctly', (tester) async {
-    // Start the app
-    await tester.pumpWidget(const MyApp());
+    // Start the app using main()
+    app.main();
+    await tester.pumpAndSettle();
+    await Future.delayed(const Duration(seconds: 1));
 
     // Verify initial counter value is 0
     expect(find.text('0'), findsOneWidget);
@@ -17,28 +19,28 @@ void main() {
 
     // Tap the increment button
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 1));
     expect(find.text('1'), findsOneWidget);
 
     // Tap the decrement button
     await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
+    await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 1));
     expect(find.text('0'), findsOneWidget);
 
     // Tap the increment button twice
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 1));
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 1));
     expect(find.text('2'), findsOneWidget);
 
     // Tap the reset button
     await tester.tap(find.byIcon(Icons.lock_reset_outlined));
-    await tester.pump();
+    await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 1));
     expect(find.text('0'), findsOneWidget);
   });
